64a4c80db143d0b9de4b4a1e72026864c64102dd
Joao Vieira <joaocv3@gmail.com>
2020-10-29 11:18:00 -0300

    Add contador iterações

--- gulosos.go
+++ gulosos.go
@@ -5,15 +5,27 @@ import (
 	"math"
 )
 
-func main() {
-	troco := troco(2.89)
-	fmt.Println(troco)
+var count int
 
+func main() {
+	count = 0
+	troco := troco(2.89)
+	fmt.Println("Resultado Troco: ", troco, " | Iterações: ", count)
+	fmt.Println()
+
+	count = 0
 	init := []int{4, 6, 13, 4, 2, 6, 7, 9, 1, 3, 9}
 	final := []int{8, 7, 14, 5, 4, 9, 10, 11, 6, 13, 12}
-	sdm := sdm(init, final, len(init))
-	fmt.Println(sdm)
+	sdmRes := sdm(init, final, len(init))
+	fmt.Println("Resultado não ordenado: ", sdmRes, " | Iterações: ", count)
+	fmt.Println()
 
+	count = 0
+	// Com valores ordenados
+	initSorted := []int{2, 4, 1, 6, 4, 6, 7, 9, 9, 3, 13}
+	finalSorted := []int{4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
+	sdmRes = sdm(initSorted, finalSorted, len(initSorted))
+	fmt.Println("Resultado ordenado: ", sdmRes, " | Iterações: ", count)
 }
 
 var availableCoins = [5]float64{1., .25, .10, .05, .01}
@@ -24,6 +36,7 @@ func troco(value float64) (coins []float64) {
 	var highestAvailableCoin float64
 
 	for sum != value {
+		count++
 		highestAvailableCoin = availableCoins[index]
 
 		if highestAvailableCoin+sum > value {
@@ -42,10 +55,13 @@ func troco(value float64) (coins []float64) {
 }
 
 func sdm(s []int, f []int, n int) (x []int) {
-	f[0] = math.MinInt8
+	f = append([]int{math.MinInt8}, f...) // adiciona valor negativo ao inicio do vetor
+	s = append([]int{math.MinInt8}, s...) // adiciona valor negativo ao inicio do vetor
 	i := 0
 
-	for k := 1; k < n; k++ {
+	for k := 1; k <= n; k++ {
+		count++
+
 		if s[k] > f[i] {
 			x = append(x, k)
 			i = k
